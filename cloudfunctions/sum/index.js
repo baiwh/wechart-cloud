caa66e42444eae85889c64a1b43161e58e8a4f95
baiwh <471974983@qq.com>
2018-09-14 16:24:15 +0800

    测试函数

--- cloudfunctions/sum/index.js
+++ cloudfunctions/sum/index.js
@@ -4,6 +4,14 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 // 云函数入口函数
-exports.main = async (event, context) => {
-
+// exports.main = async (event, context) => {
+//
+// }
+// 云函数入口函数
+exports.main = (event, context) => {
+    console.log(event)
+    console.log(context)
+    return {
+        sum: event.a + event.b
+    }
 }
