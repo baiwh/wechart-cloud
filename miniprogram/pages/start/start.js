ac7678f9088cfc94ebc4be700581a4d47f4d84f7
baiwh <471974983@qq.com>
2018-09-16 18:09:07 +0800

    开始页面

---
+++ miniprogram/pages/start/start.js
@@ -0,0 +1,58 @@
+// miniprogram/pages/start/start.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo:{},
+    avatarUrl:''
+  },
+
+  getUserInfo: function(){
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+})
