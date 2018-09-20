774461d031f116320ea10e03b67ed82ec1395888
baiwh <471974983@qq.com>
2018-09-20 17:26:59 +0800

    个人信息保存到数据库

--- miniprogram/pages/start/start.js
+++ miniprogram/pages/start/start.js
@@ -1,67 +1,66 @@
 // miniprogram/pages/start/start.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    userInfo:{},
-    avatarUrl:'./../../images/n.jpg',
-    isAuthSetting: true,
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.logged && e.detail.userInfo) {
-      this.setData({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        userInfo: {},
+        avatarUrl: './../../images/n.jpg',
         isAuthSetting: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-              let app = getApp()
-              app.globalData.userInfo = res.userInfo
-            }
-          })
+// 获取用户授权并储存
+    onGetUserInfo: function (e) {
+        if (!this.logged && e.detail.userInfo) {
+            this.setData({
+                isAuthSetting: true,
+                avatarUrl: e.detail.userInfo.avatarUrl,
+                userInfo: e.detail.userInfo
+            })
+            // 把信息储存下来
+            let app = getApp()
+            app.globalData.userInfo = e.detail.userInfo
+            // 传回数据库
+            this.onAdd(e.detail.userInfo)
         }
-      }
-    })
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  },
+    // 把用户信息储存到数据库
+    onAdd: function (info) {
+        const db = wx.cloud.database()
+        db.collection('userinfo').add({
+            data: {
+                info: info
+            }
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        // 获取用户信息
+        wx.getSetting({
+            success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            this.setData({
+                                avatarUrl: res.userInfo.avatarUrl,
+                                userInfo: res.userInfo
+                            })
+                            // 把信息储存下来
+                            let app = getApp()
+                            app.globalData.userInfo = res.userInfo
+                            // 跳转到主页
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
+                        }
+                    })
+                }
+            }
+        })
+    },
 
 })
