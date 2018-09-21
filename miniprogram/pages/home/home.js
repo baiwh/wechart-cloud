495212e0c274da7ba03a1bd940ec214a20e4de9d
baiwh <471974983@qq.com>
2018-09-21 16:57:31 +0800

    主页基本完成

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,66 +1,71 @@
 // miniprogram/pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        date: '2018-09-21',
+        time: '14:42'
+    },
 
-  },
+    bindDateChange: function (e) {
+        this.setData({
+            date: e.detail.value
+        })
+    },
+    bindTimeChange: function (e) {
+        this.setData({
+            time: e.detail.value
+        })
+    },
+    signIn: () => {
+        wx.showLoading({
+            mask:true
+        })
+        const db = wx.cloud.database()
+        db.collection('signin').add({
+            data: {
+                date: this.data.date,
+                time: this.data.time
+            },
+            success: res => {
+                wx.showToast({
+                    icon: 'success',
+                    title: '打卡成功',
+                })
+                console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+            },
+            fail: err => {
+                wx.showToast({
+                    icon: 'none',
+                    title: '打卡失败'
+                })
+                console.error('[数据库] [新增记录] 失败：', err)
+            },
+            complete:()=>{
+                wx.hideLoading()
+            }
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        // yyyy/mm/dd
+        let nowDate = new Date()
+        let year = nowDate.getFullYear()
+        let month = nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1)
+            : nowDate.getMonth() + 1
+        let day = nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate
+            .getDate()
+        let hour = nowDate.getHours()
+        let min = nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()
+        this.setData({
+            date: year + '-' + month + '-' + day,
+            time: hour + ':' + min
+        })
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
