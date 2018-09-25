abc49954b0a4a3c863be027e9c2cef1557467a81
baiwh <471974983@qq.com>
2018-09-25 17:53:19 +0800

    history的echarts

--- miniprogram/pages/history/history.js
+++ miniprogram/pages/history/history.js
@@ -1,66 +1,123 @@
 // miniprogram/pages/history/history.js
+import * as echarts from "../../ec-canvas/echarts";
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        ecBar: {
+            onInit: function (canvas, width, height) {
+                const barChart = echarts.init(canvas, null, {
+                    width: width,
+                    height: height
+                });
+                canvas.setChart(barChart);
+                barChart.setOption(getBarOption());
 
-  },
+                return barChart;
+            }
+        },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
+function getVirtulData(year) {
+    year = year || '2017';
+    var date = +echarts.number.parseDate(year + '-01-01');
+    var end = +echarts.number.parseDate((+year + 1) + '-01-01');
+    var dayTime = 3600 * 24 * 1000;
+    var data = [];
+    for (var time = date; time < end; time += dayTime) {
+        data.push([
+            echarts.format.formatTime('yyyy-MM-dd', time),
+            Math.floor(Math.random() * 1000)
+        ]);
+    }
+    return data;
+}
+function getBarOption() {
+    return {
+        tooltip : {},
+        calendar: {
+            top: 'middle',
+            left: 'center',
+            orient: 'vertical',
+            cellSize: 40,
+            yearLabel: {
+                margin: 50,
+                textStyle: {
+                    fontSize: 30
+                }
+            },
+            dayLabel: {
+                firstDay: 1,
+                nameMap: 'cn'
+            },
+            monthLabel: {
+                nameMap: 'cn',
+                margin: 15,
+                textStyle: {
+                    fontSize: 20,
+                    color: '#999'
+                }
+            },
+            range: ['2018-09', '2018-10']
+        },
+        visualMap: {
+            min: 0,
+            max: 1000,
+            type: 'piecewise',
+            left: 'center',
+            bottom: 20,
+            inRange: {
+                color: ['#5291FF', '#C7DBFF']
+            },
+            seriesIndex: [1],
+            orient: 'horizontal'
+        },
+        series: [{
+            type: 'graph',
+            edgeSymbol: ['none', 'arrow'],
+            coordinateSystem: 'calendar',
+            symbolSize: 15,
+            calendarIndex: 0,
+            itemStyle: {
+                normal: {
+                    color: 'yellow',
+                    shadowBlue: 9,
+                    shadowOffsetX: 1.5,
+                    shadowOffsetY: 3,
+                    shadowColor: '#555'
+                }
+            },
+            lineStyle: {
+                normal: {
+                    color: '#D10E00',
+                    width: 1,
+                    opacity: 1
+                }
+            },
+            z: 20
+        }, {
+            type: 'heatmap',
+            coordinateSystem: 'calendar',
+            data: getVirtulData(2018)
+        }]
+    };
+}
