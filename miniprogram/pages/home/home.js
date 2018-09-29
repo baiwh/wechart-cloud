7af703b04411fa1e50099bed649bdc5e9618837b
baiwh <471974983@qq.com>
2018-09-29 18:00:53 +0800

    写好一点

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -72,19 +72,20 @@ Page({
      */
     onLoad: function (options) {
       console.log(this)
+      this.refreshBtn()
         // yyyy/mm/dd
-        let nowDate = new Date()
-        let year = nowDate.getFullYear()
-        let month = nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1)
-            : nowDate.getMonth() + 1
-        let day = nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate
-            .getDate()
-        let hour = nowDate.getHours()
-        let min = nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()
-        this.setData({
-            date: year + '-' + month + '-' + day,
-            time: hour + ':' + min
-        })
+        // let nowDate = new Date()
+        // let year = nowDate.getFullYear()
+        // let month = nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1)
+        //     : nowDate.getMonth() + 1
+        // let day = nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate
+        //     .getDate()
+        // let hour = nowDate.getHours()
+        // let min = nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()
+        // this.setData({
+        //     date: year + '-' + month + '-' + day,
+        //     time: hour + ':' + min
+        // })
     },
 
 })
