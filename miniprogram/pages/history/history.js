8456c46c88991b7baf1d7c590a17f12d7214eb55
baiwh <471974983@qq.com>
2018-10-12 16:59:46 +0800

    修改历史记录的echarts写法

--- miniprogram/pages/history/history.js
+++ miniprogram/pages/history/history.js
@@ -1,65 +1,15 @@
 // miniprogram/pages/history/history.js
 import * as echarts from "../../ec-canvas/echarts";
 
-Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        echartsOption:{},
-        ecBar: {
-            onInit: function (canvas, width, height) {
-                const barChart = echarts.init(canvas, null, {
-                    width: width,
-                    height: height
-                });
-                canvas.setChart(barChart);
-                barChart.setOption(getBarOption());
-
-                return barChart;
-            }
-        },
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-})
-function getVirtulData(year) {
-    year = year || '2017';
-    var date = +echarts.number.parseDate(year + '-01-01');
-    var end = +echarts.number.parseDate((+year + 1) + '-01-01');
-    var dayTime = 3600 * 24 * 1000;
-    var data = [];
-    for (var time = date; time < end; time += dayTime) {
-        data.push([
-            echarts.format.formatTime('yyyy-MM-dd', time),
-            Math.floor(Math.random() * 1000)
-        ]);
-    }
-    return data;
-}
-function getBarOption() {
-    return {
+// 图表数据
+function setOption(chart) {
+    const option = {
         calendar: {
-          top: '30%',
-          left: 'center',
+            top: '30%',
+            left: 'center',
             orient: 'vertical',
             cellSize: 35,
-            itemStyle:{
+            itemStyle: {
                 fontSize: 10,
                 color: '#999'
             },
@@ -80,7 +30,7 @@ function getBarOption() {
             monthLabel: {
                 nameMap: 'cn',
                 margin: 10,
-                verticalAlign:'middle',
+                verticalAlign: 'middle',
                 textStyle: {
                     fontSize: 10,
                     color: '#999'
@@ -103,17 +53,81 @@ function getBarOption() {
             coordinateSystem: 'calendar',
             data: getVirtulData(2018),
             label: {
-              normal: {
-                show: true,
-                formatter: function (params) {
-                  var d = echarts.number.parseDate(params.value[0]);
-                  return d.getDate();
-                },
-                textStyle: {
-                  color: '#000'
+                normal: {
+                    show: true,
+                    formatter: function (params) {
+                        var d = echarts.number.parseDate(params.value[0]);
+                        return d.getDate();
+                    },
+                    textStyle: {
+                        color: '#000'
+                    }
                 }
-              }
             },
         }
     };
+    chart.setOption(option);
+}
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        echartsOption: {},
+        ec: {
+            lazyLoad: true
+        },
+    },
+    // 点击按钮后初始化图表
+    init: function () {
+        this.ecComponent.init((canvas, width, height) => {
+            // 初始化图表
+            const chart = echarts.init(canvas, null, {
+                width: width,
+                height: height
+            });
+            setOption(chart);
+            // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+            this.chart = chart;
+            this.setData({
+                isLoaded: true,
+                isDisposed: false
+            });
+            // 给图表增加点击事件
+            chart.on("click", (param) => {
+                console.log(param.data)
+                console.log(this.data)
+            })
+            return chart;
+        });
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function () {
+        // 获取组件
+        this.ecComponent = this.selectComponent('#mychart-dom-bar');
+        // 初始化图表
+        this.init()
+    },
+
+})
+
+// 日历背景颜色随机函数
+function getVirtulData(year) {
+    year = year || '2017';
+    var date = +echarts.number.parseDate(year + '-01-01');
+    var end = +echarts.number.parseDate((+year + 1) + '-01-01');
+    var dayTime = 3600 * 24 * 1000;
+    var data = [];
+    for (var time = date; time < end; time += dayTime) {
+        data.push([
+            echarts.format.formatTime('yyyy-MM-dd', time),
+            Math.floor(Math.random() * 1000)
+        ]);
+    }
+    return data;
 }
--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -4,41 +4,54 @@ Page({
     /**
      * 页面的初始数据
      */
+
     data: {
         date: '2018-09-21',
         time: '14:42'
     },
-  refreshBtn: function(){
-    // yyyy/mm/dd
-    let nowDate = new Date()
-    let year = nowDate.getFullYear()
-    let month = nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1)
-      : nowDate.getMonth() + 1
-    let day = nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate
-      .getDate()
-    let hour = nowDate.getHours()
-    let min = nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()
-    this.setData({
-      date: year + '-' + month + '-' + day,
-      time: hour + ':' + min
-    })
-  },
+
+    // 刷新按钮、获取当前时间
+    refreshBtn: function () {
+        // yyyy/mm/dd
+        let nowDate = new Date()
+        let year = nowDate.getFullYear()
+        let month = nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1)
+            : nowDate.getMonth() + 1
+        let day = nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate
+            .getDate()
+        let hour = nowDate.getHours()
+        let min = nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()
+        // 修改data
+        this.setData({
+            date: year + '-' + month + '-' + day,
+            time: hour + ':' + min
+        })
+    },
+
+    // 时间选择器-日期
     bindDateChange: function (e) {
         this.setData({
             date: e.detail.value
         })
     },
+
+    // 时间选择器-时间
     bindTimeChange: function (e) {
         this.setData({
             time: e.detail.value
         })
     },
+
+    // 打卡按钮
     signIn: function () {
+        // 打开loading
         wx.showLoading({
             mask: true
         })
+        // 获取全局变量、获取个人信息
         let app = getApp()
         console.log(app.globalData.userInfo)
+        // db初始化
         const db = wx.cloud.database()
         db.collection('signin').add({
             data: {
@@ -70,22 +83,9 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
+
     onLoad: function (options) {
-      console.log(this)
-      this.refreshBtn()
-        // yyyy/mm/dd
-        // let nowDate = new Date()
-        // let year = nowDate.getFullYear()
-        // let month = nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1)
-        //     : nowDate.getMonth() + 1
-        // let day = nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate
-        //     .getDate()
-        // let hour = nowDate.getHours()
-        // let min = nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()
-        // this.setData({
-        //     date: year + '-' + month + '-' + day,
-        //     time: hour + ':' + min
-        // })
+        this.refreshBtn()
     },
 
 })
