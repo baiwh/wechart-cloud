3aad9d610a19e282ad0d33ae5279ca361c3bf08e
baiwh <471974983@qq.com>
2018-10-30 17:51:12 +0800

    统计页面

--- miniprogram/pages/history/history.js
+++ miniprogram/pages/history/history.js
@@ -16,9 +16,80 @@ Page({
     },
     // 时间选择器-日期
     bindDateChange: function (e) {
+        // 保存选值
         this.setData({
             date: e.detail.value
         })
+        // 调取数据库
+
+        // 重绘图表
+        let option = {
+            calendar: {
+                top: '30%',
+                left: 'center',
+                orient: 'vertical',
+                cellSize: 40,
+                itemStyle: {
+                    fontSize: 10,
+                    color: '#999'
+                },
+                yearLabel: {
+                    show:false
+                },
+                dayLabel: {
+                    firstDay: 7,
+                    nameMap: 'cn',
+                    textStyle: {
+                        fontSize: 10,
+                        color: '#999'
+                    }
+                },
+                monthLabel: {
+                    show:false
+                },
+                range: [this.data.date]
+            },
+            visualMap: {
+                show: false,
+                min: 0,
+                max: 5,
+                type: 'piecewise',
+                inRange: {
+                    color: ['#ffffff', '#5291FF']
+                },
+                orient: 'horizontal'
+            },
+            series: {
+                type: 'heatmap',
+                coordinateSystem: 'calendar',
+                data: [["2018-10-01", 5],["2018-10-02", 0],["2018-10-03", 0],["2018-10-04", 0],["2018-10-05", 0],["2018-10-06", 0],
+                    ["2018-10-07", 0],["2018-10-08", 0],["2018-10-09", 0],["2018-10-10", 0],["2018-10-11", 0],
+                    ["2018-10-12", 0],["2018-10-13", 0],["2018-10-14", 0],["2018-10-15", 0],["2018-10-16", 0],
+                    ["2018-10-17", 0],["2018-10-18", 0],["2018-10-19", 0],["2018-10-20", 0],["2018-10-21", 0],
+                    ["2018-10-22", 0],["2018-10-23", 0],["2018-10-24", 0],["2018-10-25", 0],["2018-10-26", 0],
+                    ["2018-10-27", 0],["2018-10-28", 0],["2018-10-29", 0],["2018-10-30", 0],["2018-10-31", 0]],
+                label: {
+                    normal: {
+                        show: true,
+                        formatter: function (params) {
+                            let day = params.value[0].split('-')[2]
+                            let d = day.charAt(0)
+                            let data = ''
+                            if (d==='0') {
+                                data = day.charAt(1)
+                            } else{
+                                data = day
+                            }
+                            return data;
+                        },
+                        textStyle: {
+                            color: '#000'
+                        }
+                    }
+                },
+            }
+        };
+        this.init(option)
     },
     // 初始化图表
     init: function (option) {
@@ -59,11 +130,15 @@ Page({
             .getDate()
         let hour = nowDate.getHours()
         let min = nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()
+        // 保存当天日期
         this.setData({
             date: year + '-' + month
         })
         let date= year + '-' + month + '-' + day
-        //获取数据库当天记录的值，并赋值给signInData并显示，如果当天无值为空，显示加号
+        // 获取数据库当天记录的值，并赋值给signInData并显示，如果当天无值为空，显示加号
+
+        // 获取数据库中当月的数据
+
         // 初始化图表
         let option = {
             calendar: {
@@ -131,7 +206,6 @@ Page({
                 },
             }
         };
-        console.log(option)
         this.init(option)
     },
 
