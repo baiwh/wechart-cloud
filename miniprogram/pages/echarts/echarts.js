efe20a20add24362cf695c78a7ffd079a2d34b9c
baiwh <471974983@qq.com>
2018-11-30 17:51:27 +0800

    统计页面柱状图

--- miniprogram/pages/echarts/echarts.js
+++ miniprogram/pages/echarts/echarts.js
@@ -1,172 +1,112 @@
 // miniprogram/pages/echarts/echarts.js
 import * as echarts from '../../ec-canvas/echarts';
 
-let chart = null;
-
-function initChart(canvas, width, height) {
-    chart = echarts.init(canvas, null, {
-        width: width,
-        height: height
-    });
-    canvas.setChart(chart);
-
-    var option = {
-        color: ['#37a2da', '#32c5e9', '#67e0e3'],
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-            }
-        },
-        legend: {
-            data: ['热度', '正面', '负面']
-        },
-        grid: {
-            left: 20,
-            right: 20,
-            bottom: 15,
-            top: 40,
-            containLabel: true
-        },
-        xAxis: [
-            {
-                type: 'value',
-                axisLine: {
-                    lineStyle: {
-                        color: '#999'
-                    }
-                },
-                axisLabel: {
-                    color: '#666'
-                }
-            }
-        ],
-        yAxis: [
-            {
-                type: 'category',
-                axisTick: { show: false },
-                data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
-                axisLine: {
-                    lineStyle: {
-                        color: '#999'
-                    }
-                },
-                axisLabel: {
-                    color: '#666'
-                }
-            }
-        ],
-        series: [
-            {
-                name: '热度',
-                type: 'bar',
-                label: {
-                    normal: {
-                        show: true,
-                        position: 'inside'
-                    }
-                },
-                data: [300, 270, 340, 344, 300, 320, 310],
-                itemStyle: {
-                    // emphasis: {
-                    //   color: '#37a2da'
-                    // }
-                }
-            },
-            {
-                name: '正面',
-                type: 'bar',
-                stack: '总量',
-                label: {
-                    normal: {
-                        show: true
-                    }
-                },
-                data: [120, 102, 141, 174, 190, 250, 220],
-                itemStyle: {
-                    // emphasis: {
-                    //   color: '#32c5e9'
-                    // }
-                }
-            },
-            {
-                name: '负面',
-                type: 'bar',
-                stack: '总量',
-                label: {
-                    normal: {
-                        show: true,
-                        position: 'left'
-                    }
-                },
-                data: [-20, -32, -21, -34, -90, -130, -110],
-                itemStyle: {
-                    // emphasis: {
-                    //   color: '#67e0e3'
-                    // }
-                }
-            }
-        ]
-    };
-
-    chart.setOption(option);
-    return chart;
-}
 Page({
-    onShareAppMessage: function (res) {
-        return {
-            title: 'ECharts 可以在微信小程序中使用啦！',
-            path: '/pages/index/index',
-            success: function () { },
-            fail: function () { }
-        }
-    },
   /**
    * 页面的初始数据
    */
   data: {
       ec: {
-          onInit: initChart
-      }
+          lazyLoad: true
+      },
   },
-
+    // 初始化图表
+    init: function (option) {
+        this.ecComponent.init((canvas, width, height) => {
+            // 初始化图表
+            const chart = echarts.init(canvas, null, {
+                width: width,
+                height: height
+            });
+            chart.setOption(option);
+            // 将图表实例绑定到 this 上，可以在其他成员函数中访问
+            this.chart = chart;
+            this.setData({
+                isLoaded: true,
+                isDisposed: false
+            });
+            // 给图表增加点击事件
+            chart.on("click", (param) => {
+                console.log(param)
+            })
+            return chart;
+        });
+    },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      // 获取组件
+      this.ecComponent = this.selectComponent('#mychart-dom-bar');
+      // 初始化图表
+      let option = {
+          color: ['#37a2da'],
+          tooltip: {
+              trigger: 'axis',
+              axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                  type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+              }
+          },
+          legend: {
+              data: ['平均加班时长']
+          },
+          grid: {
+              left: 20,
+              right: 20,
+              bottom: 15,
+              top: 40,
+              containLabel: true
+          },
+          xAxis: [
+              {
+                  type: 'value',
+                  axisLine: {
+                      lineStyle: {
+                          color: '#999'
+                      }
+                  },
+                  axisLabel: {
+                      color: '#666'
+                  }
+              }
+          ],
+          yAxis: [
+              {
+                  type: 'time',
+                  axisTick: { show: false },
+                  data: ['七月', '六月', '五月', '四月', '三月', '二月', '一月'],
+                  axisLine: {
+                      lineStyle: {
+                          color: '#999'
+                      }
+                  },
+                  axisLabel: {
+                      color: '#666'
+                  }
+              }
+          ],
+          series: [
+              {
+                  name: '平均加班时长',
+                  type: 'bar',
+                  label: {
+                      normal: {
+                          show: true,
+                          position: 'inside'
+                      }
+                  },
+                  data: [300, 270, 340, 344, 300, 320, 310],
+                  itemStyle: {
+                      // emphasis: {
+                      //   color: '#37a2da'
+                      // }
+                  }
+              }
+          ]
+      };
+      this.init(option)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-      setTimeout(function () {
-          // 获取 chart 实例的方式
-          console.log(chart)
-      }, 2000);
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
@@ -175,17 +115,4 @@ Page({
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
